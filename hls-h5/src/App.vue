adc85b0f501cb6326ec53c0dbef8586fb8474d84
QiuShuiDyp <993865890@qq.com>
2023-06-14 08:43:21 +0800

    feat: 增加对融云的im接入

--- hls-h5/src/App.vue
+++ hls-h5/src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import { onMounted, onUnmounted, ref } from 'vue'
+import * as RongIMLib from '@rongcloud/imlib-next'
 import videoPlayer from 'video.js'
 import 'video.js/dist/video-js.css'
+const Events = RongIMLib.Events
+
 const player = ref()
-onMounted(() => {
+const imConnectStatus = ref < boolean > false
+// 初始化拉流
+function initVideoStream() {
   const video = document.getElementById('videoElement')
   const options = {
     autoplay: true,
@@ -16,6 +21,34 @@ onMounted(() => {
     ],
   }
   player.value = videoPlayer('videoElement', options)
+}
+function initRongClond() {
+  RongIMLib.init({ appKey: 'xxx' })
+
+  RongIMLib.addEventListener(Events.CONNECTING, () => {
+    console.log('正在链接服务器')
+  })
+
+  RongIMLib.addEventListener(Events.CONNECTED, () => {
+    console.log('已经链接到服务器')
+  })
+
+  RongIMLib.addEventListener(Events.MESSAGES, (evt) => {
+    console.log(evt.messages)
+  })
+  RongIMLib.connect('<Your-Token>').then((res) => {
+    if (res.code === RongIMLib.ErrorCode.SUCCESS) {
+      imConnectStatus.value = true
+      console.log('链接成功, 链接用户 id 为: ', res.data.userId)
+    } else {
+      imConnectStatus.value = false
+      console.warn('链接失败, code:', res.code)
+    }
+  })
+}
+onMounted(() => {
+  initVideoStream()
+  initRongClond()
 })
 onUnmounted(() => {
   player.dispose()
